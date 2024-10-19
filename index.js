b549a7ec56bd4d287e9283c57ccd86e754c6f2e8
subbuchatla <135306007+subbuchatla@users.noreply.github.com>
2024-10-20 00:23:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello Subbu!')
 })
 
 app.listen(port, () => {
-  console.log(Example app listening on port 3000)
+  console.log(`Example app listening on port ${port}`)
 })
